9906522d92cde487e5c68e262d7c6ce0f70f0264
zayed-04 <eng.zayed04@gmail.com>
2017-10-20 20:48:00 +0600

    DbContext assignes and db created for student.

---
+++ Lbl.Model/BusinessDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lbl.Model.Migrations;
+
+namespace Lbl.Model
+{
+    public class BusinessDbContext : DbContext
+    {
+        public BusinessDbContext() : base("DefaultBusinessConnection")
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
