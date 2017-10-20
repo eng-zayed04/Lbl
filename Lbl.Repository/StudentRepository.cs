dca378f18dabcf3dffe29ee061b2793bcf5c663e
zayed-04 <eng.zayed04@gmail.com>
2017-10-20 22:26:16 +0600

    Student models, repository done. Server porsion going..

---
+++ Lbl.Repository/StudentRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lbl.Model.Migrations;
+
+namespace Lbl.Repository
+{
+    public class StudentRepository
+    {
+        public bool Add(Student student)
+        {
+            return true;
+        }
+    }
+}
---
+++ Lbl.Service/StudentService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lbl.Model.Migrations;
+using Lbl.Repository;
+
+namespace Lbl.Service
+{
+    public class StudentService
+    {
+        public bool Add(Student student)
+        {
+            StudentRepository repository = new StudentRepository();
+            return repository.Add(student);
+        }
+    }
+}
