b241674104d852519aedb71a5743e150b9820753
zayed-04 <eng.zayed04@gmail.com>
2017-10-21 23:11:45 +0600

    Yahoo!! Problem solved. Changing terget framework of Lbl.Server to 4.6.2. Thanks to Foyzul Karim vai.

--- Lbl.Server/Controllers/StudentController.cs
+++ Lbl.Server/Controllers/StudentController.cs
@@ -4,10 +4,18 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Lbl.Model.Students;
+using Lbl.Service;
 
 namespace Lbl.Server.Controllers
 {
     public class StudentController : ApiController
     {
+        public IHttpActionResult Post(Student student)
+        {
+            StudentService service = new StudentService();
+            bool add = service.Add(student);
+            return this.Ok(add);
+        }
     }
 }
